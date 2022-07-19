4f4c09670810873b937cb1c3c4b0e729be4a434c
Tina Wen <ttw4@rice.edu>
2022-07-18 18:06:01 -0700

    added workspace

---
+++ owl-planner/src/component/Workspace.js
@@ -0,0 +1,16 @@
+import React from 'react' 
+
+const Workspace = () => {
+        // add elements to workspace.js
+        // put style in-line
+        <div style = {
+                {
+                        width: "100px"
+                }
+        } ></div>
+
+        // take out null
+        return (null)
+}
+
+export default Workspace
