cfc943e4743280c67b60364772e823260cf796e0
dasech <danielcho@Daniels-MacBook-Air.local>
2022-07-27 12:46:43 -0500

    Pushing Workspace Button

--- owl-planner/src/App.js
+++ owl-planner/src/App.js
@@ -1,22 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import WorkSpaceButton from "./components/WorkSpaceButton.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header"> 
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
+        <a  
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
+        > 
           Learn React
-        </a>
+        </a> 
+        <WorkSpaceButton/> 
       </header>
     </div>
   );
---
+++ owl-planner/src/components/WorkSpaceButton.js
@@ -0,0 +1,29 @@
+const WorkSpaceButton = () => {
+    return (
+        <div style = { 
+            {
+                width: "200px",
+                height: "35px",
+                backgroundColor: "#2A66E7",
+                borderRadius: "7px",
+            }
+        }> 
+        <div style = {
+            {
+                fontSize: "17px",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                // position: "center",
+                textAlign: "center",
+                // verticalAlign: "middle",
+                paddingTop: "7.5px",
+        
+            }
+        }> + New Workspace
+        </div>
+        </div>
+        );
+
+    }
+    export default WorkSpaceButton;
