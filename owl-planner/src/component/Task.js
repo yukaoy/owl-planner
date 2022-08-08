0fd1ec1aec40898c7a50c4a29467fa4b8ac75f04
raahimabsar <sa114@rice.edu>
2022-08-08 15:54:33 -0700

    Adding Task

--- owl-planner/src/component/Task.js
+++ owl-planner/src/component/Task.js
@@ -1,1 +1,11 @@
-a 
+const Task = ({ task }) => {
+    return (
+      <div className='task'>
+          <h3>{task.text}</h3>
+          <p>{task.day}</p>
+      </div>
+    )
+  }
+  
+  export default Task
+  
